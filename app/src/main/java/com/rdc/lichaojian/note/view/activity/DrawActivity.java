65ca1126db1a9a808c4da71da11b5a0c9da370c3
lichaojian <709921277@qq.com>
2016-08-28 16:28:52 +0800

    新增了绘制的Activity

---
+++ app/src/main/java/com/rdc/lichaojian/note/view/activity/DrawActivity.java
@@ -0,0 +1,15 @@
+package com.rdc.lichaojian.note.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rdc.lichaojian.note.activity.R;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
