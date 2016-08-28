92e3bf24508298ac4fa1e5c31a2c8cc6338bd599
lichaojian <709921277@qq.com>
2016-08-28 16:11:53 +0800

    添加了res资源文件

---
+++ app/src/main/java/com/rdc/lichaojian/note/view/widget/VerticalSeekBar.java
@@ -0,0 +1,24 @@
+package com.rdc.lichaojian.note.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+/**
+ * Created by lichaojian on 16-8-28.
+ */
+public class VerticalSeekBar extends SeekBar {
+
+    public VerticalSeekBar(Context context) {
+        super(context);
+    }
+
+    public VerticalSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
