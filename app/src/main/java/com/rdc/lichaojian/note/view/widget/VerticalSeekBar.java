344c4eedc48c0fd323b9949a16af06676d0038e6
lichaojian <709921277@qq.com>
2016-08-28 16:13:05 +0800

    添加了竖向的SeekBar

--- app/src/main/java/com/rdc/lichaojian/note/view/widget/VerticalSeekBar.java
+++ app/src/main/java/com/rdc/lichaojian/note/view/widget/VerticalSeekBar.java
@@ -1,7 +1,9 @@
 package com.rdc.lichaojian.note.view.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.SeekBar;
 
 /**
@@ -13,12 +15,47 @@ public class VerticalSeekBar extends SeekBar {
         super(context);
     }
 
+    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
     public VerticalSeekBar(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    protected void onDraw(Canvas canvas) {
+        canvas.rotate(-90);
+        canvas.translate(-getHeight(), 0);
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                setProgress(getMax() - (int) (getMax() * event.getY() / getHeight()));
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
     }
 
 }
