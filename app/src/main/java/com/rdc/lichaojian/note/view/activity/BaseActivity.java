997557e4ea8fe5a0b7da132995f54e2ef07b3d97
lichaojian <709921277@qq.com>
2016-08-28 16:05:31 +0800

    创建项目

---
+++ app/src/main/java/com/rdc/lichaojian/note/view/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.rdc.lichaojian.note.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lichaojian on 16-8-28.
+ */
+public abstract class BaseActivity extends AppCompatActivity{
+
+    public abstract void initView();
+
+}
---
+++ app/src/main/java/com/rdc/lichaojian/note/view/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rdc.lichaojian.note.view.activity;
+
+import android.os.Bundle;
+
+import com.rdc.lichaojian.note.activity.R;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
